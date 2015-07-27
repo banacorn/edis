66688723cb3fbd722aae50157ad32326ab10f74c
Ting-Yen Lai <banacorn@gmail.com>
2015-07-27 16:03:33 +0800

    now we are getting somewhere

--- src/Main.hs
+++ src/Main.hs
@@ -1,22 +1,37 @@
-{-# LANGUAGE OverloadedStrings, DeriveGeneric, DefaultSignatures #-}
+{-# LANGUAGE OverloadedStrings, DeriveGeneric, DefaultSignatures, DeriveDataTypeable #-}
 
 module Main where
 
-import Tredis
+import Tredis as T
 
 import GHC.Generics
-import Database.Redis hiding (decode)
+import Data.Typeable
+import Database.Redis (connect, defaultConnectInfo)
 import Data.ByteString
 import Data.Serialize (Serialize)
 import Control.Monad.Trans (liftIO)
 
-data L = R | G | Y deriving (Show, Generic)
+data L = R | G | Y deriving (Show, Generic, Typeable)
 instance Serialize L
 
 main :: IO ()
 main = do
     conn <- connect defaultConnectInfo
-    runRedis conn $ do
-        set "hello" (encode [R, G, G, Y])
-        a <- get "hello" >>= decode :: Redis (Either E L)
-        liftIO $ print a
+    runTredis conn $ do
+        set "hello" ([R, G, Y] :: [L]) >>= liftIO . print
+        incr "hello"
+        get "hello" >>= liftIO . (print :: Either TredisReply (Maybe [L]) -> IO ())
+
+        -- set "hello" True >>= liftIO . print
+        -- a <- get "hello" :: Tredis (Either TredisReply (Maybe Bool))
+        -- liftIO $ print a
+
+
+        -- incr "hello" >>= liftIO . print
+        -- a <- get "hello" >>= decode :: Redis (Either E L)
+        -- liftIO $ print a
+
+        -- set "hello" $ encode (Just 2 :: String)
+        -- a <- get "hello"
+        -- a <- get "hello" >>= decode :: Redis (Either E Bool)
+        -- liftIO $ print a
--- src/Tredis.hs
+++ src/Tredis.hs
@@ -1,19 +1,57 @@
 module Tredis where
 
 import Database.Redis as R hiding (decode)
+import Data.Typeable
 import Data.ByteString
 import Data.Serialize as S
-import Control.Monad.Trans (liftIO)
+-- import Control.Monad.Trans (liftIO)
+import Control.Monad.State
+import Control.Applicative ((<$>))
+import Data.Map as Map
+import Data.Map
 
-data E = ConnE Reply | SemE | DecE String deriving (Show)
+type Key = ByteString
+type TypeTable = Map Key TypeRep
+type Tredis = StateT TypeTable Redis
+data TredisReply = RedisReply Reply
+                 | DecodeError String
+                 | Undeclared Key
+                 | TypeError Key TypeRep TypeRep
+                 deriving (Show)
 
 encode :: Serialize a => a -> ByteString
 encode = S.encode
 
-decode :: Serialize a => Either Reply (Maybe ByteString) -> Redis (Either E a)
-decode (Right reply) = return $ case reply of
-    Just raw -> case S.decode raw of
-        Right val -> Right val
-        Left err  -> Left (DecE err)
-    Nothing  -> Left SemE
-decode (Left err) = return $ Left (ConnE err)
+decode :: Serialize a => Either Reply (Maybe ByteString) -> Either TredisReply (Maybe a)
+decode (Left err) = Left (RedisReply err)   -- error from Redis
+decode (Right Nothing) = Right Nothing      -- nil
+decode (Right (Just raw)) = case S.decode raw of
+    Right val -> Right (Just val)           -- ok
+    Left err  -> Left (DecodeError err)     -- decode error
+
+typeOfInteger :: TypeRep
+typeOfInteger = typeOf (0 :: Integer)
+
+mapRedisReply :: Either Reply a -> Either TredisReply a
+mapRedisReply (Left reply)  = Left $ RedisReply reply
+mapRedisReply (Right state) = Right state
+
+runTredis :: Connection -> Tredis a -> IO a
+runTredis conn f = runRedis conn $ evalStateT f Map.empty
+
+set :: (Serialize a, Typeable a) => ByteString -> a -> Tredis (Either TredisReply Status)
+set key val = do
+    modify $ insert key (typeOf val)
+    lift $ mapRedisReply <$> R.set key (Tredis.encode val)
+
+incr :: ByteString -> Tredis (Either TredisReply Integer)
+incr key = do
+    result <- gets (Map.lookup key)
+    case result of
+        Just ty -> case ty == typeOfInteger of
+            True -> lift $ mapRedisReply <$> R.incr key
+            False -> return $ Left $ TypeError key typeOfInteger ty
+        Nothing -> return $ Left $ Undeclared key
+
+get :: (Serialize a, Typeable a) => ByteString -> Tredis (Either TredisReply (Maybe a))
+get key = lift $ Tredis.decode <$> R.get key
