a9083bb45f1fccf177d20fda1a820a7296728a71
Ting-Yen Lai <banacorn@gmail.com>
2015-10-20 15:57:51 +0800

    asdf

--- sandbox/Sym.hs
+++ sandbox/Sym.hs
@@ -4,24 +4,26 @@
 
 module SymList where
 
-import Prelude hiding (lookup)
 import GHC.TypeLits
 import Data.Proxy
-import GHC.Exts (Constraint)
 
 --------------------------------------------------------------------------------
 --  kind-indexed singletons
+--
+--  Sing   
+--
 --------------------------------------------------------------------------------
-
 data family Sing (a :: k)
 
-type Nat = (Sing :: N -> *)
-type Sym = (Proxy :: Symbol -> *)
-
 --------------------------------------------------------------------------------
---  Natural Number
---------------------------------------------------------------------------------
-
+--  Natural number à la peano, promoted
+--
+--  Value-level  Type-level         Models
+--  -----------  ------------       -------
+--  SZ           Sing 'Z            0
+--  SS SZ        Sing ('S 'Z)       1
+--  SS (SS SZ)   Sing ('S ('S 'Z))  2
+--
 data N = Z | S N
 
 data instance Sing (a :: N) where
@@ -79,34 +81,6 @@ egGetTerm0 :: get (Get Z '["A", "B"]) ~ Proxy "A" => ()
 egGetTerm0 = ()
 
 --------------------------------------------------------------------------------
---  indexing on SymList
---------------------------------------------------------------------------------
-
--- type level function
-type family Index (s :: Symbol) (xs :: [Symbol]) :: N where
-    Index s (s ': xs) = Z
-    Index s (x ': xs) = S (Index s xs)
-
--- example of Index
-egIndex0 :: Index "A" '["A", "B"] ~ Z => ()
-egIndex0 = ()
-
-egIndex1 :: Index "B" '["A", "B"] ~ (S Z) => ()
-egIndex1 = ()
-
--- term level function
-class SIndex s xs where
-    index :: Proxy s -> SymList xs -> Sing (Index s xs)
-
-instance SIndex s (s ': xs) where
-    index _ _ = SZ
-
-instance (   Index s (x ': xs) ~ S (Index s xs)     -- relation
-         ,                         SIndex s xs)
-         => SIndex s (x ': xs) where
-    index s (SymCons x xs) = SS $ index s xs
-
---------------------------------------------------------------------------------
 --  Searching for a Symbol on a SymList
 --------------------------------------------------------------------------------
 
@@ -138,67 +112,3 @@ type family FromJust (x :: Maybe k) :: k where
 
 egFromJust0 :: (FromJust (Just Char) ~ Char) => ()
 egFromJust0 = ()
-
---------------------------------------------------------------------------------
---  Dictionary
---------------------------------------------------------------------------------
-
-data Dict :: [ (Symbol, *) ] -> * where
-    DNil :: Dict '[]
-    DCons :: Proxy s    -- key
-             -> x       -- value
-             -> Dict ts -- old dict
-             -> Dict ('(s, x) ': ts) -- new dict
-
-egDict0 :: Dict '[]
-egDict0 = DNil
-
-egDict1 :: Dict '[ '("A", Char), '("B", Int) ]
-egDict1 = DCons Proxy 'a' (DCons Proxy 0 DNil)
-
---------------------------------------------------------------------------------
---  Dictionary Membership
---------------------------------------------------------------------------------
-
-type family Member (s :: Symbol) (xs :: [ (Symbol, *) ]) :: Bool where
-    Member s '[]             = False
-    Member s ('(s, x) ': xs) = True
-    Member s ('(t, x) ': xs) = Member s xs
-
-egMember0 :: (Member "C" '[ '("A", Char), '("B", Int) ] ~ False) => ()
-egMember0 = ()
-
---------------------------------------------------------------------------------
---  Dictionary Insertion/Update
---------------------------------------------------------------------------------
-
-type family Insert (s :: Symbol) (x :: *) (xs :: [ (Symbol, *) ]) :: [ (Symbol, *) ] where
-    Insert s x '[]             = '[ '(s, x) ]
-    Insert s x ('(s, y) ': xs) = ('(s, x) ': xs)
-    Insert s x ('(t, y) ': xs) = '(t, y) ': (Insert s x xs)
-
---------------------------------------------------------------------------------
---  Dictionary Lookup
---------------------------------------------------------------------------------
-
-type family Lookup (s :: Symbol) (xs :: [ (Symbol, *) ]) :: Maybe * where
-    Lookup s '[]             = Nothing
-    Lookup s ('(s, x) ': xs) = Just x
-    Lookup s ('(t, x) ': xs) = Lookup s xs
-
-egLookup0 :: (Lookup "A" '[ '("A", Char), '("B", Int) ] ~ Just Char) => ()
-egLookup0 = ()
---
--- egLookup1 :: (Lookup "A" '[ '("A", Char), '("B", Int) ] ~ Just Char) => ()
--- egLookup1 = ()
-
-class SLookup s xs where
-    lookup :: Proxy s -> Dict xs -> FromJust (Lookup s xs)
-
-instance SLookup s ('(s, x) ': xs) where
-    lookup s (DCons _ x _) = x
-
-instance (  (Lookup s ('(t, x) ': xs)) ~ (Lookup s xs)
-         ,                               SLookup s xs)
-         => SLookup s ('(t, x) ': xs) where
-    lookup s (DCons _ _ xs) = lookup s xs
